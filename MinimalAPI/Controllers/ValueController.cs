0c5c6b08ee031938bf5c7ea8501d199832410acf
İlhan Enes Daniş <danisenes3@gmail.com>
2025-07-08 22:17:32 +0300

    first commit

---
+++ MinimalAPI/Controllers/ValueController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MinimalAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public sealed class ValueController : ControllerBase
+    {
+
+    }
+}
